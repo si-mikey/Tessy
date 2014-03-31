0b23a45a47d21c43eb884af35a15445f3819fb10
Luis Lopez <llopez@shutterstock.com>
2014-03-31 12:43:17 -0400

    framework structure fixes

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 var express = require('express');
+var routes = require('./lib/routes');
+var user = require('./lib/routes/user');
+var http = require('http');
+var path = require('path');
+
 var app = express();
 
+// all environments
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'lib/views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-  res.send('hello world');
+app.get('/', routes.index);
+app.get('/users', user.list);
 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Tessy started on port %d ', app.get('port'));
 });
 
+//var server = app.listen(8000, function(){
 
-app.get('/index', function(req, res){
+ //console.log('Tessy Started on port %d', server.address().port);
 
-  res.send('hello world');
-
-});
-
-
-var server = app.listen(8000, function(){
-
- console.log('Tessy Started on port %d', server.address().port);
-
-});
+//});
 
 
 
