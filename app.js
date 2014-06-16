e683ed9a192906edca137d5fd0a00ff47ef4a5db
Luis <mikeylopez@gmail.com>
2014-06-16 00:54:09 -0400

    footer fixes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var tests = require('./lib/routes/testcases.js');
 var reports = require('./lib/routes/reports.js');
 var manage = require('./lib/routes/manage.js');
 
-
 var app = express();
 
 // all environments
@@ -14,10 +13,12 @@ app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'lib/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
+app.use(express.cookeParser());
+app.use(express.session({secret: '123456789'});
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
+//app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,8 +33,6 @@ app.get('/index', home.index);
 app.get('/tests', tests.index);
 app.get('/reports', reports.index);
 app.get('/manage', manage.index);
-
-//console.log(home, tests, reports, manage);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Tessy started on port %d ', app.get('port'));
