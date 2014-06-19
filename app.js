35cd3334109c85d16a5f3b57fd7d47d5172ea3b3
Luis <mikeylopez@gmail.com>
2014-06-19 01:06:34 -0400

    process login route setup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var db      = require('mariasql');
 
 var routes    = require('./lib/routes');
 
+
 var app = express();
 
 // all environments
@@ -17,6 +18,7 @@ app.use(express.session({secret: '123456789'}));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+//app.use(express.bodyParser());
 //app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,6 +36,7 @@ app.get('/testcases', routes.testcases);
 app.get('/reports', routes.reports);
 app.get('/manage', routes.manage);
 app.get('/login', routes.login);
+app.post('/dologin', routes.dologin);
 
 //db connection
 //var dbobj = new db();
@@ -49,7 +52,7 @@ app.get('/login', routes.login);
 //   console.log('db connected');
 //
 //});
-//
+
 //start the server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Tessy started on port %d ', app.get('port'));
