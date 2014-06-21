0914c78ab7db9dcdf4b17e1c899e70454e39d9a1
Luis <mikeylopez@gmail.com>
2014-06-21 19:59:21 -0400

    start shortcut and new db table

--- lib/sql/tessy_init.sql
+++ lib/sql/tessy_init.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE IF NOT EXISTS `tessy`
-
 -- ---
 -- Globals
 -- ---
@@ -21,15 +19,33 @@ CREATE TABLE `accounts` (
 );
 
 -- ---
+-- Table 'users'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `users`;
+		
+CREATE TABLE `users` (
+  `id` SMALLINT NULL AUTO_INCREMENT DEFAULT NULL,
+  `user_name` VARCHAR(40) NULL DEFAULT NULL,
+  `first_name` VARCHAR(50) NULL DEFAULT NULL,
+  `last_name` VARCHAR(50) NULL DEFAULT NULL,
+  `password_hash` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
 -- Foreign Keys 
 -- ---
 
+ALTER TABLE `accounts` ADD FOREIGN KEY (user_id) REFERENCES `users` (`id`);
 
 -- ---
 -- Table Properties
 -- ---
 
 -- ALTER TABLE `accounts` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
@@ -37,4 +53,6 @@ CREATE TABLE `accounts` (
 
 -- INSERT INTO `accounts` (`id`,`user_id`) VALUES
 -- ('','');
+-- INSERT INTO `users` (`id`,`user_name`,`first_name`,`last_name`,`password_hash`) VALUES
+-- ('','','','','');
 
