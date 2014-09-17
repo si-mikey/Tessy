8388d0fa042f23b9e05f142abc887bf6bf8102f7
Luis <mikeylopez@gmail.com>
2014-09-17 02:37:41 -0400

    chnage dropdown text to selected value based on route

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,36 @@
-//Main js file
+var tessy = window.tessy || {}; 
+  
+  var lists = document.querySelectorAll(".nav li");
+  arr = [];
+  for(var i = lists.length; i--; arr.unshift(lists[i]));
+  arr.forEach(function(item){ 
 
-var lists = document.querySelectorAll(".nav li");
-arr = [];
-for(var i = lists.length; i--; arr.unshift(lists[i]));
-arr.forEach(function(item){ 
+      if( item.childNodes[0].parentNode.className == 'active' ) item.childNodes[0].parentNode.className = ''; 
+      if( window.location.href == item.childNodes[0] ) item.childNodes[0].parentNode.className = 'active' 
+  });
 
-    if( item.childNodes[0].parentNode.className == 'active' ) item.childNodes[0].parentNode.className = ''; 
-    if( window.location.href == item.childNodes[0] ) item.childNodes[0].parentNode.className = 'active' 
-});
 
+
+tessy = {
+
+  getUrlParam : function(name){
+
+     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+     results = regex.exec(location.search);
+     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " ")); 
+
+   },
+
+  paramsToDropDowns : function(){
+
+    var params = window.location.pathname.split("/").slice(2,5);
+    if( params.length > 0){
+      params.map(function(param, index){ 
+        $(".span10 button")[index].innerHTML = param + '<span class="caret"></span>';
+      });
+    } 
+
+  }
+
+};
